871b577cac184a9fc82380a20f7acf4f13367132
Clifford18 <cliffordjohn.cj@gmail.com>
2022-11-23 15:36:47 +0300

    update Greet.js file

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -3,5 +3,7 @@ import React from 'react';
 // function Greet() {
 // 	return <h1>Hello CJ</h1>;
 // }
-const Greet = () => <h1>Hello CJ</h1>;
-export default Greet;
+export const Greet = () => <h1>Hello CJ</h1>;
+
+
+	// export default Greet;
