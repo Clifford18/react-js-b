1c8e506a97bc06f5481560e429c0b9f95de3377f
Clifford18 <cliffordjohn.cj@gmail.com>
2022-11-23 18:20:14 +0300

    update App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import Greet from "./components/Greet";
+import {Greet} from "./components/Greet";
+import {Welcome} from "./components/Welcome";
 
 function App() {
-  return (
-    <div className="App">
-      <Greet/>
+	return (
+		<div className="App">
+			<Greet/>
+			<Welcome/>
 
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
